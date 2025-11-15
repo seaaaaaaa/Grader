4dbb22d7e796ed2f08e83026cd6a3247fb2e9fb6
seaaaaaaa <paramee.seaza@gmail.com>
2025-11-16 02:58:29 +0700

    gen optimal bst

--- subtree_extract/student.h
+++ subtree_extract/student.h
@@ -8,13 +8,30 @@
 template <typename KeyT,typename MappedT, typename CompareT >
 size_t CP::map_bst<KeyT,MappedT,CompareT>::process(node* n) {
   //write your code here
-  return 0;
+  if(n == NULL) return 0;
+  return process(n->left) + process(n->right) + 1;
 }
 
 template <typename KeyT,typename MappedT, typename CompareT >
 std::pair<KeyT,MappedT> CP::map_bst<KeyT,MappedT,CompareT>::subtree(map_bst<KeyT,MappedT,CompareT> &left, map_bst<KeyT,MappedT,CompareT> &right) {
   //write your code here
-  return std::pair<KeyT,MappedT>();
+  if(mSize == 0) {
+    left.clear();
+    right.clear();
+    return std::pair<KeyT,MappedT>(KeyT(), MappedT());
+  }
+  left.clear();
+  right.clear();
+  left.mSize = process(mRoot->left);
+  right.mSize = process(mRoot->right);
+  left.mRoot = mRoot->left;
+  right.mRoot = mRoot->right;
+  if(left.mRoot != NULL) left.mRoot->parent = NULL;
+  if(right.mRoot != NULL) right.mRoot->parent = NULL;
+  mRoot->left = NULL;
+  mRoot->right = NULL;
+  mSize = 1;
+  return std::pair<KeyT,MappedT>(mRoot->data.first, mRoot->data.second);
 }
 
 #endif
