f1dd4e03447fbfc0c1c4610dc995e89bd613020c
seaaaaaaa <paramee.seaza@gmail.com>
2025-11-19 03:16:27 +0700

    list check update

--- list_check/student.h
+++ list_check/student.h
@@ -1,26 +1,48 @@
 #ifndef __STUDENT_H__
 #define __STUDENT_H__
 
+#include "list.h"
+#include <stack>
 
 template <typename T>
 bool CP::list<T>::check() {
-  //your code here
-  if(mSize==0) return mHeader->next==mHeader && mHeader->prev==mHeader;
-  if(mHeader->next == NULL || mHeader->prev == NULL) return false;
-  if(mHeader->next->prev != mHeader) return false;
-  if(mHeader->prev->next != mHeader) return false;
-  int count = 0;
-  node* p = mHeader->next;
-  while(p != mHeader) {
-    if(p == NULL) return false;
-    if(p->prev == NULL || p->next == NULL) return false;
-    if(p->prev->next != p) return false;
-    p = p->next;
-    count++;
-    if(count > mSize) return false;
-  }
-  return count == mSize;
-}
+    //your code here
 
+    int count = 0;
+    auto node = mHeader;
+    std::stack<T> vals;
+    vals.push(mHeader->data);
+
+    // forward check
+    do {
+
+        node = node->next;
+        ++count;
+        if (!node) return false;
+        vals.push(node->data);
+        
+    } while (node != mHeader && count < mSize+1);
+
+    if (count != mSize+1 || node != mHeader) return false;
+
+    // reset
+    count = 0;
+    vals.pop();
+
+    // backward check
+    do {
+
+        node = node->prev;
+        ++count;
+        if (!node || node->data != vals.top()) return false;
+        vals.pop();
+
+    } while (node != mHeader && count < mSize+1);
+
+    if (count != mSize+1 || node != mHeader) return false;
+
+    return true;
+
+}
 
 #endif
